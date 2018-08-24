7c01b9dc0e9048b0aee0510e157fed8c5a5717cb
Kim Pham <pham.kim.a@gmail.com>
2018-08-24 19:32:15 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,76 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |info_hash|
+    if info_hash["status"] == "Winner"
+      return info_hash["name"].split.first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, info_array|
+    info_array.each do |info_hash|
+      if info_hash["occupation"] == occupation
+        return info_hash["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  
+  data.each do |season, info_array|
+    info_array.each do |info_hash|
+      if info_hash["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, info_array|
+    if info_array["hometown"] == hometown
+      
 end
 
 def get_average_age_for_season(data, season)
   # code here
 end
+
+data1 = { 
+  "season 30": [
+    {
+      "name":      "Beth Smalls",
+      "age":       "26",
+      "hometown":  "Great Falls, Virginia",
+      "occupation":"Nanny/Freelance Journalist",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Becca Tilley",
+      "age":        "27",
+      "hometown":   "Shreveport, Louisiana",
+      "occupation": "Chiropractic Assistant",
+      "status":     "Eliminated Week 8"
+    }
+  ],
+  "season 29": [
+    {
+      "name":      "Ashley Yeats",
+      "age":       "24",
+      "hometown":  "Denver, Colorado",
+      "occupation":"Dental Assitant",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Sam Grover",
+      "age":        "29",
+      "hometown":   "New York, New York",
+      "occupation": "Entertainer",
+      "status":     "Eliminated Week 6"
+    }
+  ]
+}
